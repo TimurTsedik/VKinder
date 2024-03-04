6c558921a814d6edabcf7ed417c622b96cd6c0ed
“Vlad <vladvolkov71@gmail.com>
2024-03-04 12:00:08 +0300

    vk_id, fav_id, bl_id перевел в integer

--- DB/ManageDB.py
+++ DB/ManageDB.py
@@ -18,7 +18,7 @@ class ManageDB:
     def add_user(self, user_info: dict) -> bool:
         """Добавление пользователя в базу\n
         Параметры:\n
-        Vk_id - short string\n
+        Vk_id - integer\n
         Name - string\n
         Age - integer > 0\n
         Gender is Male = 1, Female = 0\n
@@ -39,12 +39,12 @@ class ManageDB:
                      age=user_info['age'],
                      sex=user_info['sex'],
                      city=user_info['city'],
-                     foto_a_1 = user_info['foto_a_1'],
-                     foto_a_2 = user_info['foto_a_2'],
-                     foto_a_3 = user_info['foto_a_3'],
-                     foto_fr_1 = user_info['foto_fr_1'],
-                     foto_fr_2 = user_info['foto_fr_2'],
-                     foto_fr_3 = user_info['foto_fr_3']))
+                     foto_a_1=user_info['foto_a_1'],
+                     foto_a_2=user_info['foto_a_2'],
+                     foto_a_3=user_info['foto_a_3'],
+                     foto_fr_1=user_info['foto_fr_1'],
+                     foto_fr_2=user_info['foto_fr_2'],
+                     foto_fr_3=user_info['foto_fr_3']))
             self._session.commit()
             return True
 
@@ -57,7 +57,8 @@ class ManageDB:
         Gender is Male = 1, Female = 0\n
         City - INTEGER\n
         Возвращает True если юзер обновлен в базе.\n"""
-        if self.get_user_by_vk_id(user_info['vk_id']) is None or datetime.now() - self.get_user_by_vk_id(user_info['vk_id'])['date_create'] > timedelta(days=1):
+        if self.get_user_by_vk_id(user_info['vk_id']) is None or datetime.now() - \
+                self.get_user_by_vk_id(user_info['vk_id'])['date_create'] > timedelta(days=1):
             x_ret = self._session.query(User).where(User.vk_id == user_info['vk_id'])
             x_ret.update(user_info)
             self._session.commit()
@@ -65,7 +66,7 @@ class ManageDB:
         else:
             return False
 
-    def add_favorites(self, user_id: str, fav_id: str) -> bool:
+    def add_favorites(self, user_id: int, fav_id: int) -> bool:
         """Добавление пользователя с базу избранных\n
         Parameters:\n
         user_id кто добавляет в базу\n
@@ -82,7 +83,7 @@ class ManageDB:
             self._session.commit()
             return True
 
-    def remove_favorites(self, user_id: str, fav_id: str) -> bool:
+    def remove_favorites(self, user_id: int, fav_id: int) -> bool:
         """Удаление пользователя из избранных\n
         Parameters:\n
         user_id кто удаляет из базы\n
@@ -92,7 +93,7 @@ class ManageDB:
         self._session.commit()
         return True
 
-    def add_blacklist(self, user_id: str, bl_id: str) -> bool:
+    def add_blacklist(self, user_id: int, bl_id: int) -> bool:
         """Добавление пользователя черный список\n
         Parameters:\n
         user_id кто добавляет в базу\n
@@ -109,7 +110,7 @@ class ManageDB:
             self._session.commit()
             return True
 
-    def remove_blacklist(self, user_id: str, bl_id: str) -> bool:
+    def remove_blacklist(self, user_id: int, bl_id: int) -> bool:
         """Удаление пользователя из черного списка\n
         Parameters:\n
         user_id кто удаляет из базы\n
@@ -119,7 +120,7 @@ class ManageDB:
         self._session.commit()
         return True
 
-    def get_list_favorites(self, vk_id: str) -> list:
+    def get_list_favorites(self, vk_id: int) -> list:
         """Получение списка избранных\n
         Возвращает не пустой LIST, если все хорошо."""
         fav_list = []
@@ -131,7 +132,7 @@ class ManageDB:
         except:
             return fav_list
 
-    def get_list_blacklist(self, vk_id: str) -> list:
+    def get_list_blacklist(self, vk_id: int) -> list:
         """Получение черного списка\n
         Возвращает не пустой LIST, если все хорошо."""
         bl_list = []
@@ -143,15 +144,15 @@ class ManageDB:
         except:
             return bl_list
 
-    def get_user_by_vk_id(self, vk_id: str) -> dict:
+    def get_user_by_vk_id(self, vk_id: int) -> dict:
         """Получение словаря с данными юзера\n
         Возвращает не пустой LIST, если все хорошо."""
         x_ret = self._session.query(User).where(User.vk_id == vk_id)
         for x in x_ret.all():
             return {"name": x.name, "surname": x.surname, "age": x.age, "sex": x.sex, "city": x.city, "vk_id": x.vk_id,
                     "date_create": x.date_create,
-            "foto_a_1": x.foto_a_1, "foto_a_2": x.foto_a_2, "foto_a_3": x.foto_a_3, "foto_fr_1": x.foto_fr_1,
-            "foto_fr_2": x.foto_fr_2, "foto_fr_3": x.foto_fr_3}
+                    "foto_a_1": x.foto_a_1, "foto_a_2": x.foto_a_2, "foto_a_3": x.foto_a_3, "foto_fr_1": x.foto_fr_1,
+                    "foto_fr_2": x.foto_fr_2, "foto_fr_3": x.foto_fr_3}
         return {}
 
 
@@ -162,16 +163,18 @@ if __name__ == '__main__':
     DB = ManageDB(db_name=config['DB']['DB_name'], user_name=config['DB']['DB_user'],
                   user_password=config['DB']['DB_password'])
 
-    DB.add_user({"name": "Vasya", "surname": "Pupkin", "age": 18, "sex": 1, "city": 1, "vk_id": "1", "foto_a_1": "1", "foto_a_2": "2",
-         "foto_a_3": "3", "foto_fr_1": "4", "foto_fr_2": "5", "foto_fr_3": "6"})
-    DB.add_user({"name": "Petya", "surname": "Ivanov", "age": 55, "sex": 1, "city": 1, "vk_id": "2", "foto_a_1": "1", "foto_a_2": "2",
-         "foto_a_3": "3", "foto_fr_1": "4", "foto_fr_2": "5", "foto_fr_3": "6"})
+    DB.add_user({"name": "Vasya", "surname": "Pupkin", "age": 18, "sex": 1, "city": 1, "vk_id": 1, "foto_a_1": "1",
+                 "foto_a_2": "2",
+                 "foto_a_3": "3", "foto_fr_1": "4", "foto_fr_2": "5", "foto_fr_3": "6"})
+    DB.add_user({"name": "Petya", "surname": "Ivanov", "age": 55, "sex": 1, "city": 1, "vk_id": 2, "foto_a_1": "1",
+                 "foto_a_2": "2",
+                 "foto_a_3": "3", "foto_fr_1": "4", "foto_fr_2": "5", "foto_fr_3": "6"})
     DB.add_user(
-        {"name": "V", "surname": "P", "age": 20, "sex": 1, "city": 1, "vk_id": "3", "foto_a_1": "1", "foto_a_2": "2",
+        {"name": "V", "surname": "P", "age": 20, "sex": 1, "city": 1, "vk_id": 3, "foto_a_1": "1", "foto_a_2": "2",
          "foto_a_3": "3", "foto_fr_1": "4", "foto_fr_2": "5", "foto_fr_3": "6"})
-    DB.actualize_user({"name": "Vasya", "surname": "Pupkin", "age": 38, "sex": 1, "city": 1, "vk_id": "1"})
-    DB.add_favorites("1", "2")
+    DB.actualize_user({"name": "Vasya", "surname": "Pupkin", "age": 38, "sex": 1, "city": 1, "vk_id": 1})
+    DB.add_favorites(1, 2)
     # DB.add_blacklist("1", "3")
-    print(DB.get_list_favorites("1"))
-    print(DB.get_list_blacklist("1"))
-    print(DB.get_user_by_vk_id("1"))
+    print(DB.get_list_favorites(1))
+    print(DB.get_list_blacklist(1))
+    print(DB.get_user_by_vk_id(1))
--- DB/ModelBD.py
+++ DB/ModelBD.py
@@ -6,7 +6,7 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
-    vk_id = sa.Column(sa.VARCHAR(128), primary_key=True)
+    vk_id = sa.Column(sa.Integer, primary_key=True)
     name = sa.Column(sa.VARCHAR(128), nullable=False)
     surname = sa.Column(sa.VARCHAR(128))
     city = sa.Column(sa.VARCHAR(128))
@@ -24,16 +24,16 @@ class User(Base):
 class Favorite(Base):
     __tablename__ = "users_favorites"
     id = sa.Column(sa.Integer, primary_key=True)
-    user_id = sa.Column(sa.VARCHAR(128), sa.ForeignKey("users.vk_id"), nullable=False)
-    user_fav_id = sa.Column(sa.VARCHAR(128), sa.ForeignKey("users.vk_id"), nullable=False)
+    user_id = sa.Column(sa.Integer, sa.ForeignKey("users.vk_id"), nullable=False)
+    user_fav_id = sa.Column(sa.Integer, sa.ForeignKey("users.vk_id"), nullable=False)
     # favorite = relationship("User", backref="favorite")
 
 
 class BlackList(Base):
     __tablename__ = "black_list"
     id = sa.Column(sa.Integer, primary_key=True)
-    user_id = sa.Column(sa.VARCHAR(128), sa.ForeignKey("users.vk_id"), nullable=False)
-    user_black_id = sa.Column(sa.VARCHAR(128), sa.ForeignKey("users.vk_id"), nullable=False)
+    user_id = sa.Column(sa.Integer, sa.ForeignKey("users.vk_id"), nullable=False)
+    user_black_id = sa.Column(sa.Integer, sa.ForeignKey("users.vk_id"), nullable=False)
     # black_list = relationship("User", backref="black_list")
 
 
