e22086ef6803fb57997acc769a16307c9f9a5e03
timur555 <timur.tsedik@gmail.com>
2024-03-04 14:09:22 +0200

    добавлена ссылка на профиль в выдаче и аттач фоточек

--- main.py
+++ main.py
@@ -28,21 +28,21 @@ def start_vk_bot(token_1, token_2):
 
 
 
-    def write_msg(user_id, mess, keyboard=None):
+    def write_msg(user_id, mess, keyboard=None, attachment=None):
         if keyboard is not None:
             vk.method('messages.send', {
-            'user_id': user_id, 'message': mess, 'random_id': randrange(10 ** 7), 'keyboard': keyboard})
+            'user_id': user_id, 'message': mess, 'random_id': randrange(10 ** 7), 'keyboard': keyboard, 'attachment': attachment})
         else:
             vk.method('messages.send', {
-                'user_id': user_id, 'message': mess, 'random_id': randrange(10 ** 7)})
+                'user_id': user_id, 'message': mess, 'random_id': randrange(10 ** 7), 'attachment': attachment})
 
     user_results = UserResultsStorage()
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
             if event.to_me:
                 bot = VkBot(event.user_id, token_1, token_2, user_results, DB)
-                message, keyboard = bot.execute_command(event.text)
-                write_msg(event.user_id, message, keyboard)
+                message, keyboard, attachment = bot.execute_command(event.text)
+                write_msg(event.user_id, message, keyboard, attachment)
 
 
 
